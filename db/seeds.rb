01e5feb3a79d912fe422741c4a154fd8446a2552
Hock Tan <hock.tan712@gmail.com>
2016-01-25 21:49:14 -0800

    Checkpoint 34:completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
   )
 end
 
@@ -9,11 +18,12 @@ posts = Post.all
 
 100.times do
   Comment.create!(
-  post: posts.sample,
-  body: RandomData.random_paragraph
+    post: posts.sample,
+    body: RandomData.random_paragraph
   )
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
